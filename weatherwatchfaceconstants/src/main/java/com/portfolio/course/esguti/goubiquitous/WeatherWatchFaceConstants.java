b31a072cb51d85e226e25cdd187bf9d8b597c8c4
Esteban Gutierrez <esteban.gutierrez@cmcc.it>
2016-03-14 10:48:54 +0100

    Change package name

---
+++ weatherwatchfaceconstants/src/main/java/com/portfolio/course/esguti/goubiquitous/WeatherWatchFaceConstants.java
@@ -0,0 +1,12 @@
+package com.portfolio.course.esguti.goubiquitous;
+
+public class WeatherWatchFaceConstants {
+    public static final String KEY_WEATHER_CLEAR = "CLEAR";
+    public static final String KEY_WEATHER_CLOUDY = "CLOUDY";
+    public static final String KEY_WEATHER_FOG = "FOG";
+    public static final String KEY_WEATHER_LIGHT_CLOUDS = "LIGHT_CLOUDS";
+    public static final String KEY_WEATHER_LIGHT_RAIN = "RAIN";
+    public static final String KEY_WEATHER_RAIN = "RAIN";
+    public static final String KEY_WEATHER_SNOW = "SNOW";
+    public static final String KEY_WEATHER_STORM = "STORM";
+}
